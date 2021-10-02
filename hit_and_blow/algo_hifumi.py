f9b81cca80c593afad2738b18a60939ca94e8e3b
Shun Tanaka <56306978+makuo911@users.noreply.github.com>
2021-10-02 16:53:01 +0900

    Revert "[refactor] mypy型チェックを踏まえた微修正"
    This reverts commit 192cf2b465ea7ca082d866d9aeffd4c057baad3e.

--- hit_and_blow/algo_hifumi.py
+++ hit_and_blow/algo_hifumi.py
@@ -2,9 +2,7 @@ from matplotlib import pyplot as plt
 import itertools
 import random
 import numpy as np
-num_list = ["0", "1", "2", "3", "4", "5", "6",
-            "7", "8", "9", "a", "b", "c", "d", "e", "f"]
-
+num_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
 
 def hit_blow(guess, answer):
     hit = 0
@@ -13,9 +11,8 @@ def hit_blow(guess, answer):
         if guess[i] == answer[i]:
             hit += 1
         elif guess[i] in answer:
-            blow += 1
+                blow += 1
     return [hit, blow]
-
 
 def make_ans_list(num_list):
     ans_list_tuple = list(itertools.permutations(num_list, 5))
@@ -24,36 +21,34 @@ def make_ans_list(num_list):
         ans_list.append(i[0]+i[1]+i[2]+i[3]+i[4])
     return ans_list
 
-
 def narrow_ans_list(guess, result, ans_list):
     new_ans_list = []
     length = len(ans_list)
     for ans in ans_list[:length+1]:
         h_b = hit_blow(guess, ans)
-        if h_b == result:  # もし3hit 2blowなら
-            new_ans_list.append(ans)  # 候補リストに加える
-        else:  # 3hit 2blowじゃないなら
-            pass  # なにもしない
-    ans_list = new_ans_list  # できた正解候補をもとのリストと置き換える
+        if h_b == result: # もし3hit 2blowなら
+            new_ans_list.append(ans) # 候補リストに加える
+        else: # 3hit 2blowじゃないなら
+            pass # なにもしない
+    ans_list = new_ans_list # できた正解候補をもとのリストと置き換える
     return ans_list
 
 
 # 実行してみる
-ans_digit_list = random.sample(num_list, 5)  # 答えをランダムに決める
-ans = ''.join(ans_digit_list)
+ans = random.sample(num_list, 5) # 答えをランダムに決める
+ans = ans[0]+ans[1]+ans[2]+ans[3]+ans[4]
 print(f"正解は{ans}")
 print("----------------------")
-guess = "12345"  # 初期予測（適当）
+guess = "12345" # 初期予測（適当）
 
-ans_list = make_ans_list(num_list)  # いったん全候補をリストにぶちこむ
+ans_list = make_ans_list(num_list) # いったん全候補をリストにぶちこむ
 count = 0
 while True:
     count += 1
     result = hit_blow(guess, ans)
     ans_list = narrow_ans_list(guess, result, ans_list)
-    print(guess + "と予想して残り" + str(len(ans_list)) + "パターン" +
-          "---------" + "{}hit{}blow".format(result[0], result[1]))
-    guess = ans_list[0]  # 正解候補の中から適当に（とりあえず最初の項）次の予測を決める
+    print(guess + "と予想して残り" + str(len(ans_list)) + "パターン" + "---------" + "{}hit{}blow".format(result[0],result[1]))
+    guess = ans_list[0] # 正解候補の中から適当に（とりあえず最初の項）次の予測を決める
     if result == [5, 0]:
         break
 
--- hit_and_blow/hit_and_blow_auto.py
+++ hit_and_blow/hit_and_blow_auto.py
@@ -6,32 +6,25 @@ from typing import Tuple
 
 URL = "https://damp-earth-70561.herokuapp.com"
 session = requests.Session()
-num_list = ["0", "1", "2", "3", "4", "5", "6",
-            "7", "8", "9", "a", "b", "c", "d", "e", "f"]
-players = {"C": "290d6313-c662-45b6-82d6-6afb631ade08",
-           "C2": "3cd613ef-a0c0-447c-bc23-81dcf1648be9"}
+num_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+players = {"C":"290d6313-c662-45b6-82d6-6afb631ade08", 
+        "C2":"3cd613ef-a0c0-447c-bc23-81dcf1648be9"}
 i_am = "C"
 
 # 指定した対戦部屋情報の取得
-
-
-def get_room(room_id: int, URL: str = URL, session=session) -> dict:
+def get_room(room_id:int, URL:str=URL, session=session) -> dict:
     url_get_room = URL + "/rooms/" + str(room_id)
     result = session.get(url_get_room)
     return result.json()
 
 # 部屋IDの入力
-
-
 def input_roomID():
     room_id_str = input("入室したい対戦部屋のIDを入力してください -->>")
     room_id = int(room_id_str)
     return room_id
 
 # 部屋の情報を取得して，状況を返す
-
-
-def check_room(room_id: int):
+def check_room(room_id:int):
     get_room_result = get_room(room_id)
     if get_room_result["state"] == -1:
         print("部屋には誰もいません")
@@ -44,8 +37,6 @@ def check_room(room_id: int):
         return False
 
 # 部屋に入室するか選択する
-
-
 def ask_enter_room():
     while True:
         ans = input("入室しますか？[y/n] -->>")
@@ -57,47 +48,38 @@ def ask_enter_room():
             print("y/n以外の入力がされました．入力をやり直してください")
 
 # 対戦部屋へユーザーを登録
-
-
-def enter_room(room_id: int, i_am: str = i_am, URL: str = URL, session=session) -> None:
-    headers = {"Content-Type": "application/json"}
+def enter_room(room_id:int, i_am:str=i_am, URL:str=URL, session=session) -> None:
+    headers = {"Content-Type" : "application/json"}
     url_enter_room = URL + "/rooms"
     post_data = {
         "player_id": players[i_am],
         "room_id": room_id
-    }
+        }
     result_post = session.post(url_enter_room, headers=headers, json=post_data)
     print(result_post.json())
 
-
 def input_hidden(num_list) -> str:
     hidden_number = random.sample(num_list, 5)
-    hidden_number = hidden_number[0]+hidden_number[1] + \
-        hidden_number[2]+hidden_number[3]+hidden_number[4]
-    return str(hidden_number)
+    hidden_number = hidden_number[0]+hidden_number[1]+hidden_number[2]+hidden_number[3]+hidden_number[4]
+    return hidden_number
 
 # 相手に当てさせる番号をサーバーに送る
-
-
-def post_hidden(room_id: int, i_am: str = i_am, URL: str = URL, session=session) -> str:
-    headers = {"Content-Type": "application/json"}
-    url_get_table = URL + "/rooms/" + \
-        str(room_id) + "/players/" + i_am + "/hidden"
-
+def post_hidden(room_id:int, i_am:str=i_am, URL:str=URL, session=session) -> str:
+    headers = {"Content-Type" : "application/json"}
+    url_get_table = URL + "/rooms/" + str(room_id) + "/players/" + i_am + "/hidden"
+    
     while True:
         hidden_number = input_hidden(num_list)
         post_data = {
             "player_id": players[i_am],
             "hidden_number": hidden_number
         }
-        result_post = session.post(
-            url_get_table, headers=headers, json=post_data)
+        result_post = session.post(url_get_table, headers=headers, json=post_data)
         if result_post.status_code == 200:
             print(f"{hidden_number}を相手に当ててもらいます")
             return hidden_number
         else:
             print("無効な入力がされました")
-
 
 def run_first_half() -> Tuple[int, str]:
     """プログラム前半（ID入力から相手が当てる数字を入力まで）
@@ -106,9 +88,9 @@ def run_first_half() -> Tuple[int, str]:
     :return: 入室したルームID，相手が当てる数字
     """
     enter = ""
-    while enter != True:  # 部屋に入るまで繰り返す
+    while enter != True: # 部屋に入るまで繰り返す
         room = ""
-        while room != True:  # 入れる部屋を選ぶまで繰り返す
+        while room != True: # 入れる部屋を選ぶまで繰り返す
             room_id = input_roomID()
             room = check_room(room_id)
         enter = ask_enter_room()
@@ -120,36 +102,27 @@ def run_first_half() -> Tuple[int, str]:
     return room_id, hidden_number
 
 # 対戦情報テーブル(現在のターン, hit&blowの履歴, 勝敗の判定)を取得する
-
-
-def get_table(room_id: int, i_am: str = i_am, URL: str = URL, session=session) -> dict:
-    url_get_table = URL + "/rooms/" + \
-        str(room_id) + "/players/" + i_am + "/table"
+def get_table(room_id:int, i_am:str=i_am, URL:str=URL, session=session) -> dict:
+    url_get_table = URL + "/rooms/" + str(room_id) + "/players/" + i_am + "/table"
     table_result = session.get(url_get_table)
     return table_result.json()
 
 # 勝敗が決まったかチェック
-
-
-def check_win(table_result: dict) -> bool:
+def check_win(table_result:dict) -> bool:
     if table_result["state"] == 2:
-        return True  # →ゲーム続行
+        return True # →ゲーム続行
     else:
-        return False  # →ゲーム終了
+        return False # →ゲーム終了
 
 # 自分のターンかチェック
-
-
-def check_turn(table_result, i_am: str = i_am) -> bool:
+def check_turn(table_result, i_am:str=i_am) -> bool:
     if table_result["now_player"] == i_am:
-        return True  # →自分のターン
+        return True # →自分のターン
     else:
-        return False  # →相手のターン
+        return False # →相手のターン
 
 # ゲーム終了
-
-
-def end_game(table_result, i_am: str = i_am):
+def end_game(table_result, i_am:str=i_am):
     if table_result["winner"] == i_am:
         print("あなたの勝ちです！おめでとう！")
     elif table_result["winner"] == None:
@@ -157,9 +130,7 @@ def end_game(table_result, i_am: str = i_am):
     else:
         print("あなたの負けです...")
 
-# ----------------------------------アルゴリズム
-
-
+#----------------------------------アルゴリズム
 def hit_blow(guess, answer):
     hit = 0
     blow = 0
@@ -167,9 +138,8 @@ def hit_blow(guess, answer):
         if guess[i] == answer[i]:
             hit += 1
         elif guess[i] in answer:
-            blow += 1
+                blow += 1
     return [hit, blow]
-
 
 def make_ans_list(num_list):
     ans_list_tuple = list(itertools.permutations(num_list, 5))
@@ -178,48 +148,44 @@ def make_ans_list(num_list):
         ans_list.append(i[0]+i[1]+i[2]+i[3]+i[4])
     return ans_list
 
-
 def narrow_ans_list(guess, result, ans_list):
     new_ans_list = []
     length = len(ans_list)
     for ans in ans_list[:length+1]:
         h_b = hit_blow(guess, ans)
-        if h_b == result:  # もし3hit 2blowなら
-            new_ans_list.append(ans)  # 候補リストに加える
-        else:  # 3hit 2blowじゃないなら
-            pass  # なにもしない
-    ans_list = new_ans_list  # できた正解候補をもとのリストと置き換える
+        if h_b == result: # もし3hit 2blowなら
+            new_ans_list.append(ans) # 候補リストに加える
+        else: # 3hit 2blowじゃないなら
+            pass # なにもしない
+    ans_list = new_ans_list # できた正解候補をもとのリストと置き換える
     return ans_list
-
 
 def get_result(room_id):
     table = get_table(room_id)
     result = [table["table"][-1]["hit"], table["table"][-1]["blow"]]
     return table, result
 
-# ---------------------------------
+#---------------------------------
 
 
 # 推測した数字を登録する
-def post_guess(room_id: int, guess_number: str, i_am: str = i_am, URL: str = URL, session=session):
-    headers = {"Content-Type": "application/json"}
-    url_post_guess = URL + "/rooms/" + \
-        str(room_id) + "/players/" + i_am + "/table/guesses"
-    post_data = {"player_id": players[i_am], "guess": guess_number}
+def post_guess(room_id:int, guess_number:str, i_am:str=i_am, URL:str=URL, session=session):
+    headers = {"Content-Type" : "application/json"}
+    url_post_guess = URL + "/rooms/" + str(room_id) + "/players/" + i_am + "/table/guesses"
+    post_data = {"player_id": players[i_am],"guess": guess_number}
     result_post = session.post(url_post_guess, headers=headers, json=post_data)
-
 
 def run_second_half(room_id, num_list):
     guess_number = "12345"
     ans_list = make_ans_list(num_list)
-    while True:  # 無限ループ
+    while True: # 無限ループ
         turn = False
-        while turn != True:  # 自分のターンじゃない間ずっと
+        while turn != True: # 自分のターンじゃない間ずっと
             table_result = get_table(room_id)
-            if check_win(table_result) == False:  # 勝者がいたらゲームを終わる
+            if check_win(table_result) == False: # 勝者がいたらゲームを終わる
                 end_game(table_result)
                 return
-            else:  # 勝者がいないならゲーム続行．自分のターンかチェックするループを回す．
+            else: # 勝者がいないならゲーム続行．自分のターンかチェックするループを回す．
                 turn = check_turn(table_result)
         # ↓自分のターンなら
         post_guess(room_id, guess_number)
@@ -228,11 +194,9 @@ def run_second_half(room_id, num_list):
         guess_number = ans_list[0]
         print(table["table"][-1])
 
-
 def main():
     room_id, hidden_number = run_first_half()
     run_second_half(room_id, num_list)
-
 
 if __name__ == "__main__":
     main()
