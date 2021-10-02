a5e9123cf51257bcceb6a8d1108ef815cbd540ef
a-sean-s <aoi1507soccer@gmail.com>
2021-10-03 03:44:24 +0900

    [change] 追加htmlファイルに対応した全体の修正

--- Django/user/urls.py
+++ Django/user/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('home/', views.HomeView.as_view(), name='home')   # python manage.py runserverを実行する際に, urlの後ろに'/home'をくっつける
+    path('sign/', views.Sign_in_View.as_view(), name='sign'),   # python manage.py runserverを実行する際に, urlの後ろに'/home'をくっつける
+    path('cal/', views.Calculate_View.as_view(), name='cal'), 
+    path('bmi/', views.Bmi_View.as_view(), name='bmi'),
 ]
--- Django/user/views.py
+++ Django/user/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
-class HomeView(TemplateView):
-    template_name = "user/index.html"
+class Sign_in_View(TemplateView):
+    template_name = "user/index_sign.html"
+
+class Calculate_View(TemplateView):
+    template_name = "user/index_cal.html"
+
+class Bmi_View(TemplateView):
+    template_name = "user/index_bmi.html"
