ce3c57984ff1bafa8284dfb4f33f82532a38614e
a-sean-s <aoi1507soccer@gmail.com>
2021-10-04 12:33:33 +0900

    [add] hit_and_blow 用のhtmlファイル作成

--- Django/user/views.py
+++ Django/user/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
+class Hit_and_Blow_View(TemplateView):
+    template_name = "user/index.html"
 class Sign_in_View(TemplateView):
     template_name = "user/index_sign.html"
 
