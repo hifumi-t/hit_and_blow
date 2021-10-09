e7f60c68a54c999f598b456d30d131c0e15ab841
a-sean-s <aoi1507soccer@gmail.com>
2021-10-09 14:21:57 +0900

    [update] ゲーム画面のhtmlファイル名変更

--- Django/user/views.py
+++ Django/user/views.py
@@ -1,14 +1,15 @@
+#import requests
+#from static.python.game import HitAndBlow
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
 class Hit_and_Blow_View(TemplateView):
-    template_name = "user/index.html"
-class Sign_in_View(TemplateView):
+    template_name = "user/index_game.html"
+class Sign_in_View(TemplateView):  
     template_name = "user/index_sign.html"
 
 class Calculate_View(TemplateView):
     template_name = "user/index_cal.html"
-
 class Bmi_View(TemplateView):
     template_name = "user/index_bmi.html"
