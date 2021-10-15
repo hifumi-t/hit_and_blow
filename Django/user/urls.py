b4a979a1f0e6a4dbc965022a12c439d1debed097
a-sean-s <aoi1507soccer@gmail.com>
2021-10-15 17:10:02 +0900

    [add] ゲームオーバー画面追加

--- Django/user/urls.py
+++ Django/user/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('cal/', views.Calculate_View.as_view(), name='cal'), 
     path('bmi/', views.Bmi_View.as_view(), name='bmi'),
     path('game/', views.Hit_and_Blow_View.as_view(), name='game'),
+    path('over/', views.Game_Over.as_view(), name='over'),
 ]
